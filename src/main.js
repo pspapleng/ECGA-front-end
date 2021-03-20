2892b6aff61a002f551c8fa16a8a904a2bdece3f
pspapleng <62070039@kmitl.ac.th>
2021-03-21 00:38:55 +0700

    add icon

--- src/main.js
+++ src/main.js
@@ -5,8 +5,14 @@ import router from "./router";
 import store from "./store";
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-Vue.use(Buefy);
+library.add(fas);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.use(Buefy, { defaultIconPack: "fas" });
+
 Vue.config.productionTip = false;
 
 new Vue({
