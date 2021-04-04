6aecb88d0af930c72f281440b97e24fcdc0bc3a8
fordntp <62070065@kmitl.ac.th>
2021-04-04 22:24:56 +0700

    Update login.vue

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,46 +1,31 @@
 <template>
-  <div class="hero is-fullheight bg">
-    <section class="section is-small">
+  <div class="hero is-fullheight is-light">
+    <section class="section is-medium">
       <div class="login">
-        <strong>E-CGA</strong>
-        <h3>Electronic Comprehensive Geriatric Assessment</h3>
+        <h1 id="head">เข้าสู่ระบบ <mark>E-CGA</mark></h1>
       </div>
-      <form class="box boxlogin has-background-grey-light column is-8">
+      <form class="boxlogin column is-4">
         <div class="field">
-          <h1 class="label labellog">Email</h1>
           <div class="control">
-            <input
-              class="input"
-              type="email"
-              placeholder="e.g. Rat_banyang@example.com"
-            />
+            <input class="input" type="email" placeholder="รหัสบัญชีผู้ใช้" />
           </div>
         </div>
 
         <div class="field">
-          <h1 class="label labellog">Password</h1>
           <div class="control">
-            <input class="input" type="password" placeholder="********" />
+            <input class="input" type="password" placeholder="รหัสผ่าน" />
           </div>
         </div>
-        <button class="button bt_log">
-          LOGIN
-        </button>
-        <div class="deline">
-          <svg height="10" width="500">
-            <line
-              x1="0"
-              y1="0"
-              x2="500"
-              y2="0"
-              style="stroke:rgb(360,360,360);stroke-width:4"
-            />
-          </svg>
+        <div class="forget level-right">
+          <p>ลืมรหัสผ่าน?</p>
         </div>
-        <button class="button bt_yellow">
-          CREATE NEW ACCOUNT
+        <button class="button bt-blue">
+          เข้าสู่ระบบ
         </button>
       </form>
+      <p class="newacc">
+        สมัครบัญชีพยาบาลใหม่
+      </p>
     </section>
   </div>
 </template>
@@ -61,16 +46,6 @@ export default {
   color: white;
   margin-bottom: 50px;
 }
-strong {
-  color: white;
-  font-size: 3rem;
-}
-.bg {
-  background-color: #1e3a8a;
-}
-.has-background-gray {
-  background-color: #c4c4c4;
-}
 .boxlogin {
   margin: auto;
 }
@@ -81,22 +56,27 @@ strong {
   justify-content: left;
   text-align: left;
 }
-.bt_log {
-  color: white;
-  border: none;
-  background-color: #047857;
-  padding: 0px 150px 0px 150px;
+#head {
+  color: black;
 }
-.deline {
-  padding-top: 8px;
-  margin: auto;
-  justify-content: center;
-  text-align: center;
+.bt-blue {
+  margin-top: 20px;
+  padding: 20px 80px 20px 80px;
+  color: #fff;
+  background-color: #1e3a8a;
 }
-.bt_yellow {
-  border: none;
-  padding: 0px 85px 0px 85px;
-  background-color: #fcd34d;
-  margin-bottom: 20px;
+.forget {
+  padding-right: 20px;
+  font-size: 0.75rem;
+  cursor: pointer;
+}
+.newacc {
+  color: #047857;
+  text-decoration: underline;
+  cursor: pointer;
+}
+mark {
+  background-color: yellow;
+  color: black;
 }
 </style>
